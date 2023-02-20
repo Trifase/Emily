6f8856f2292c4287b34d8c1a63a7decf5aa96a95
Luca Bellanti <luca.bellanti@gmail.com>
2023-02-20 23:49:41 +0100

    continued working on spotdl

--- develop.py
+++ develop.py
@@ -1,4 +1,19 @@
-import spotdl
+from spotdl import Spotdl
 
 TEST_URL_1 = 'https://open.spotify.com/track/4cQSmw5ZwXXh8mJCa5L42S'
-TEST_URL_2 = 'https://open.spotify.com/album/4VzzEviJGYUtAeSsJlI9QB?highlight=spotify:track:6QgjcU0zLnzq5OrUoSZ3OK '
+TEST_URL_2 = 'https://open.spotify.com/album/4VzzEviJGYUtAeSsJlI9QB?highlight=spotify:track:6QgjcU0zLnzq5OrUoSZ3OK'  # non funziona con spotdl
+TEST_URL_3 = 'https://open.spotify.com/album/1IduB3mfHISAtpV1zQHpef?si=VoaJHvv-SD-EOkq2XvZ5Xw'  # restituisce una lista di url, una per traccia
+TEST_URL_4 = 'https://open.spotify.com/track/56evvdIIJ3wVuBbeNm3ckS?si=wBMGfKEqSomjVwawWYVjJQ'
+TEST_URL_5 = 'https://open.spotify.com/track/2DtXBP9JLhsCEBexYKgHKQ'
+
+# Questi erano dentro il codice di spotdl
+client_id = "5f573c9620494bae87890c0f08a60293"
+client_secret = "212476d9b0f3472eaa762d90b19b0ba8"
+
+spotdl = Spotdl(client_id, client_secret)
+
+songs = spotdl.search([TEST_URL_5])
+
+results = spotdl.get_download_urls(songs)
+
+print(results)
